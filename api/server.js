0e875612cc0f9f0ffe03710114d1513b46965cc3
Kand-h <hshuttleworth0@gmail.com>
2020-11-29 22:28:04 +0000

    Update about page

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const nodemailer = require('nodemailer')
+require('dotenv').config();
 
 const log = console.log;
 const app = express();
@@ -14,13 +15,13 @@ function send_email(name, message_body, email, phone, subject) {
     let transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: 'hs.portfoliocontact@gmail.com',
-            pass: 'T84?yrS@5'
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD
         }
     });
 
     let mailOptions = {
-        from: 'hs.portfoliocontact@gmail.com',
+        from: process.env.EMAIL,
         to: 'hshuttleworth0@gmail.com',
         subject: 'Portfolio enquiry',
         text: "Name: " + name + ", Email: " + email + ",Subject: " + subject + ", Message Body: " + message_body + ", Phone: " + phone
